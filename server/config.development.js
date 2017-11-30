9ba8fdabc153128b7d8fb4a985acfbc9fad209cd
Steven <kbs4x4@gmail.com>
2017-11-30 10:20:23 +0000

    apply mongodb datasource

---
+++ server/config.development.js
@@ -0,0 +1,11 @@
+// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+module.exports = {
+  hostname: process.env.C9_HOSTNAME,
+  host : process.env.IP
+};
